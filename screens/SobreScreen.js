d2f840ecc168a17c26254edf7513ead1b323be3b
pschenrique <paulohenriquerj22@hotmail.com>
2026-06-11 19:16:54 -0300

    Update SobreScreen.js

--- screens/SobreScreen.js
+++ screens/SobreScreen.js
@@ -27,7 +27,7 @@ export default function SobreScreen() {
       </Text>
 
       <Text style={{ marginTop: 10, fontSize: 16 }}>
-        A ONG Amigo Fiel nasceu com a missão de transformar vidas: tanto de animais quanto de pessoas. Nosso trabalho é dedicado a oferecer um lar amoroso para cães e gatos de raça que foram abandonados, assim como para os vira-latas que encontramos nas ruas em situação de vulnerabilidade.
+        O abrigo Amigo Fiel nasceu com a missão de transformar vidas: tanto de animais quanto de pessoas. Nosso trabalho é dedicado a oferecer um lar amoroso para cães e gatos de raça que foram abandonados, assim como para os vira-latas que encontramos nas ruas em situação de vulnerabilidade.
 
 Com uma equipe apaixonada e voluntários comprometidos, resgatamos, cuidamos e disponibilizamos para adoção animais que merecem uma segunda chance. Cada adoção é um ato de amor que ajuda a reduzir o abandono e promove a conscientização sobre a importância da responsabilidade com os pets.
 
